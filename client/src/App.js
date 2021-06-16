e62dc151ad6cf47363d69b752dae84fa2b2723dc
Deepak <themdk01@gmail.com>
2021-06-16 19:24:01 +0400

    install reactrouterdom, set up basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
+import { Home, Login } from "./views";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">This is the header component</header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
     </div>
   );
 };
---
+++ client/src/views/index.js
@@ -0,0 +1,3 @@
+// Common views
+export { default as Home } from "./Home";
+export { default as Login } from "./auth/Login";
