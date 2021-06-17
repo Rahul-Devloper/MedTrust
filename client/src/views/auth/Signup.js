77e9b4a17b1f0564e3ccd3b6e83e753059595127
Deepak <themdk01@gmail.com>
2021-06-17 14:27:41 +0400

    add: sign up view

--- client/src/views/auth/Signup.js
+++ client/src/views/auth/Signup.js
@@ -1,21 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-const Register = () => {
+import {
+  EntryCard,
+  InputGroup,
+  Input,
+  Button,
+  EntryPage,
+  PageHeader,
+} from "../../components";
+const Signup = () => {
   return (
     <>
-      <h1>Login Page</h1>
-      <span>
-        Already have an account?
-        <Link to="/login">Log in</Link>
-      </span>
-      <br />
-      <span>
-        Go back
-        <Link to="/">Home</Link>
-      </span>
+      <EntryPage>
+        <PageHeader to="/">SaaS Logo</PageHeader>
+        <EntryCard>
+          <h2>Sign up</h2>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <InputGroup>
+              <label htmlFor="signup-name">Full name</label>
+              <Input type="text" placeholder="John Doe" id="signup-name" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="signup-email">Email address</label>
+              <Input type="text" placeholder="Enter email" id="signup-email" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="signup-password">Password</label>
+              <Input
+                type="password"
+                placeholder="Enter password"
+                id="signup-password"
+              />
+            </InputGroup>
+            <Button type="submit">Sign up</Button>
+          </form>
+          <span>
+            Already have an account?
+            <Link to="/login">Log in</Link>
+          </span>
+        </EntryCard>
+      </EntryPage>
     </>
   );
 };
 
-export default Register;
+export default Signup;
