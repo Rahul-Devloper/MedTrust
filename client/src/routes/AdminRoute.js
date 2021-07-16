3a961acd9a0dd6633bfd15bb980f1c9582b8ef64
dPacc <themdk01@gmail.com>
2021-07-16 20:36:35 +0400

    add: check for acctoken, if admin set admin cookie, else remove admin cookie

--- client/src/routes/AdminRoute.js
+++ client/src/routes/AdminRoute.js
@@ -3,30 +3,42 @@ import { Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { LoadingToRedirect } from "../components";
 import { currentAdmin } from "../api/admin";
+import Cookies from "js-cookie";
 
 const AdminRoute = ({ children, ...restProps }) => {
   const { auth } = useSelector((state) => ({ ...state }));
   const [ok, setOk] = useState(false);
+  const accessToken = Cookies.get("access");
+  const isAdmin = Cookies.get("is_admin");
 
+  // Check if current user is admin
   useEffect(() => {
     if (auth && auth.token) {
       currentAdmin()
-        .then((result) => {
+        .then(() => {
           setOk(true);
+          Cookies.set("is_admin", true);
         })
         .catch((error) => {
           setOk(false);
-          console.log(error);
+          console.log("ADMIN_ROUTE_ERROR", error);
         });
     }
   }, [auth]);
 
+  // Removes admin cookie
+  const removeAdminCookie = () => {
+    if (accessToken === undefined) {
+      Cookies.remove("is_admin");
+    }
+  };
+
   return (
     <>
-      {ok && auth.token ? (
+      {isAdmin && accessToken ? (
         <Route {...restProps} render={children} />
       ) : (
-        <LoadingToRedirect />
+        (removeAdminCookie(), (<LoadingToRedirect />))
       )}
     </>
   );
