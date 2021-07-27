f449e4486f3957abc575983c4d986d5102856e77
dPacc <themdk01@gmail.com>
2021-07-27 18:23:32 +0400

    move: redux store to redux folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
 import { GlobalStyle } from "./globalCSS";
 import { BrowserRouter as Router } from "react-router-dom";
-
-import { reducers } from "./reducers";
 import App from "./App";
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// redux
+import DataProvider from "./redux/store";
+
+const browserHistory = createBrowserHistory();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
+  <DataProvider>
+    <Router history={browserHistory}>
       <GlobalStyle />
       <App />
     </Router>
-  </Provider>,
+  </DataProvider>,
   document.getElementById("root")
 );
