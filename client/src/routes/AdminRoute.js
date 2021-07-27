6cff53a9c0a4238831640022c09a24fb6f116515
dPacc <themdk01@gmail.com>
2021-07-27 17:57:14 +0400

    remove: get new token utils function

--- client/src/routes/AdminRoute.js
+++ client/src/routes/AdminRoute.js
@@ -4,20 +4,11 @@ import { useSelector } from "react-redux";
 import { LoadingToRedirect } from "../components";
 import { currentAdmin } from "../api/admin";
 import Cookies from "js-cookie";
-import { getNewToken } from "../utils/getNewToken";
 
 const AdminRoute = ({ children, ...restProps }) => {
   const { auth } = useSelector((state) => ({ ...state }));
   const [ok, setOk] = useState(false);
   const accessToken = Cookies.get("access");
-
-  // If access token doesn't exist, get new access token
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getNewToken();
-    }, process.env.REACT_APP_ACCESS_TOKEN_UPDATE_INTERVAL);
-    return () => clearInterval(interval);
-  }, []);
 
   // Check if current user is admin
   useEffect(() => {
--- client/src/routes/UserRoute.js
+++ client/src/routes/UserRoute.js
@@ -2,17 +2,8 @@ import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { LoadingToRedirect } from "../components";
 import Cookies from "js-cookie";
-import { getNewToken } from "../utils/getNewToken";
 const UserRoute = ({ children, ...restProps }) => {
   const accessToken = Cookies.get("access");
-
-  // If access token doesn't exist, get new access token
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getNewToken();
-    }, process.env.REACT_APP_ACCESS_TOKEN_UPDATE_INTERVAL);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <>
