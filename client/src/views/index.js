ec72336feb655fd8aeefd40a13b5c624be82aa32
dPacc <themdk01@gmail.com>
2022-01-02 21:47:27 +0400

    add: new password view

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -3,7 +3,7 @@ export { default as Login } from "./auth/login/Login";
 export { default as Signup } from "./auth/signup/Signup";
 export { default as AccountActivation } from "./auth/AccountActivation";
 export { default as ForgotPassword } from "./auth/forgotPassword/ForgotPassword";
-export { default as NewPassword } from "./auth/NewPassword";
+export { default as NewPassword } from "./auth/newPassword/NewPassword";
 
 // User views
 export { default as Dashboard } from "./user/Dashboard";
