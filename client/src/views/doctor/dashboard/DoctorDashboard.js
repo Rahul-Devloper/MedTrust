79f02666c8faebb0cecdf2d62155fa427179c25d
Rahul-Devloper <rahulramesh26@yahoo.com>
2024-08-14 21:03:35 +0100

    added patient and doctor dashboard

--- client/src/views/doctor/dashboard/DoctorDashboard.js
+++ client/src/views/doctor/dashboard/DoctorDashboard.js
@@ -1,20 +1,33 @@
-import { Col, Row } from 'antd'
+import { Col, Row, Card, Progress, Tag } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { ArrowRightOutlined } from '@ant-design/icons'
 import { getDoctorProfileDataAction } from '../../../redux/actions/patientActions'
 import ProfileCard2 from '../../../components/Cards/ProfileCard2'
-import CardGrid from '../../../components/Cards/CardGrid'
-
 import MaleAvatar from '../../../assets/images/illustrations/doctorMaleAvatar.png'
 import FemaleAvatar from '../../../assets/images/illustrations/doctorFemaleAvatar.png'
 import MenuFooter from '../../../layouts/components/footer'
+import { getAllReviewsAction } from '../../../redux/actions/reviewActions'
+import moment from 'moment'
+import { RiSearch2Fill } from 'react-icons/ri'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryTheme,
+} from 'victory'
 
 const DoctorDashboard = () => {
   const { user } = useSelector((state) => state.auth)
   const [ok, setOk] = useState(false)
   const [doctorData, setDoctorData] = useState([])
+  const [reviewsList, setReviewsList] = useState([])
+  const history = useHistory()
 
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(
       getDoctorProfileDataAction({
@@ -25,18 +38,47 @@ const DoctorDashboard = () => {
       })
     )
 
-    // dispatch(getAllReviewsAction({ setOk, setReviewsList, physicianId }))
+    dispatch(
+      getAllReviewsAction({
+        setOk,
+        setReviewsList,
+        physicianId: user?.gmcNumber,
+      })
+    )
   }, [dispatch, user?.name, user?.gmcNumber])
 
   const paragraph = {
     label: 'Your Care Commitment',
     value: doctorData?.professionalInfo?.careCommitment,
   }
-  console.log(
-    'doctorImgUrl==>',
-    user?.ImgUrl ||
-      (doctorData?.personalInfo?.gender === 'Male' ? MaleAvatar : FemaleAvatar)
+
+  const averageRating =
+    reviewsList.length > 0
+      ? reviewsList.reduce((acc, item) => acc + item.rating, 0) /
+        reviewsList.length
+      : 0
+
+  const customDoctorFormat = (value) => (
+    <span style={{ fontSize: '24px', color: 'black' }}>
+      {reviewsList?.length}
+    </span>
   )
+
+  const handleChildClick = (child) => {
+    const formattedChild = child.trim().replace(/\s+/g, '-')
+    history.push(`/doctor/${formattedChild}`)
+  }
+
+  // Prepare data for the bar chart
+  const getReviewHistoryData = (reviews) => {
+    return reviews.map((review) => ({
+      month: moment(review.date).format('MMM YYYY'),
+      rating: review.rating,
+    }))
+  }
+
+  const reviewHistoryData = getReviewHistoryData(reviewsList)
+
   return (
     <>
       <Row gutter={[16, 16]}>
@@ -47,20 +89,21 @@ const DoctorDashboard = () => {
             }}>
             Welcome, {user?.name.split(' ')[0]} 👋
           </h2>
+        </Col>
+      </Row>
+
+      {/* First Row: Profile Card on left, Patient Reviews on right */}
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
           <ProfileCard2
             avatar={
-              doctorData?.ImgUrl ||
+              user?.ImgUrl ||
               (doctorData?.personalInfo?.gender === 'Male'
                 ? MaleAvatar
                 : FemaleAvatar)
             }
             title={`${doctorData?.personalInfo?.name}, ${doctorData?.personalInfo?.degree}`}
             description={doctorData?.professionalInfo?.specialty}
-            // ratingData={{
-            //   label: 'Overall Effectiveness',
-            //   value: doctorData?.ratings?.overallEffectiveness || 0,
-            //   color: 'magenta',
-            // }}
             details={[
               {
                 label: 'GMC Number',
@@ -71,38 +114,106 @@ const DoctorDashboard = () => {
                 value: doctorData?.professionalInfo?.hospitalAffiliations[0],
               },
             ]}
-            progressBars={[
-              {
-                label: `Your Overall Rating`,
-                value: doctorData?.ratings?.overallEffectiveness || 0,
-                color: 'magenta',
-              },
-            ]}
             progressBarSize='100px'
             paragraph={paragraph}
           />
         </Col>
-      </Row>
-      <h2>Select what you want to do</h2>
-      <Row gutter={[16, 16]} justify='center'>
-        <Col span={10}>
-          <CardGrid
-            children={[
-              'Patient Reviews',
-              'Performance Metrics',
-              //   'Average Rating',
-              //   'Your Profile',
-            ]}
-            gridStyle={{ width: '50%', textAlign: 'center' }}
-            // targetOffset={targetOffset}
-            isSticky={false}
-            viewPath={'/doctor/'}
-            toRenderView={true}
-            isHorizontal={true}
-            alignment='center'
-          />
+
+        <Col span={12}>
+          <Card
+            style={{ cursor: 'pointer' }}
+            onClick={() => handleChildClick('patient-reviews')}>
+            <Row align='middle'>
+              <Col span={4}>
+                <Progress
+                  strokeColor={{
+                    '0%': '#108ee9',
+                    '100%': '#87d068',
+                  }}
+                  type='circle'
+                  percent={100}
+                  width={150}
+                  format={customDoctorFormat}
+                />
+              </Col>
+              <Col span={16} style={{ textAlign: 'center' }}>
+                <h3>Check Out your Reviews </h3>
+              </Col>
+              <Col span={4} style={{ textAlign: 'right' }}>
+                <ArrowRightOutlined />
+              </Col>
+            </Row>
+          </Card>
         </Col>
       </Row>
+
+      {/* Second Row: Average Rating on left, Review History on right */}
+      <Row gutter={[16, 16]} style={{ marginTop: '24px' }}>
+        <Col span={12}>
+          {reviewsList.length > 0 ? (
+            <Card style={{ marginTop: '20px' }}>
+              <h3>Average Rating</h3>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Progress
+                  type='circle'
+                  percent={averageRating * 20} // Assuming ratings are out of 5, scale to 100%
+                  format={() => `${averageRating.toFixed(1)} / 5`}
+                  width={150} // Adjust size to make it larger
+                  strokeWidth={10} // Adjust the stroke width
+                />
+              </div>
+            </Card>
+          ) : (
+            <Card title='Average Rating'>
+              <p>No average rating as there are no reviews available yet.</p>
+            </Card>
+          )}
+          <Card hoverable bordered={false} style={{ marginTop: '20px' }}>
+            <p>Your last activity was on:</p>
+            <Tag color='blue'>
+              {moment(user?.lastActive).format('MMMM Do YYYY, h:mm:ss a')}
+            </Tag>
+          </Card>
+        </Col>
+
+        <Col span={12}>
+          {reviewsList.length > 0 ? (
+            <Card>
+              <h3>Review History</h3>
+              <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={20}
+                height={250} // Adjust the height as needed
+                padding={{ top: 20, bottom: 40, left: 50, right: 50 }}>
+                <VictoryAxis
+                  tickFormat={reviewHistoryData.map((item) => item.month)}
+                  style={{ tickLabels: { fontSize: 10, angle: -45 } }}
+                />
+                <VictoryAxis
+                  dependentAxis
+                  tickFormat={(x) => `${x}`}
+                  style={{ tickLabels: { fontSize: 10 } }}
+                />
+                <VictoryBar
+                  data={reviewHistoryData}
+                  x='month'
+                  y='rating'
+                  labels={({ datum }) => `Rating: ${datum.rating}`}
+                  labelComponent={<VictoryTooltip />}
+                  style={{
+                    data: { fill: '#4c72b0' },
+                  }}
+                />
+              </VictoryChart>
+            </Card>
+          ) : (
+            <Card title='Review History'>
+              <p>No reviews history available yet.</p>
+            </Card>
+          )}
+        </Col>
+      </Row>
+
       <br />
       <MenuFooter />
     </>
--- client/src/views/patient/dashboard/PatientDashboard.js
+++ client/src/views/patient/dashboard/PatientDashboard.js
@@ -1,14 +1,66 @@
-import React from 'react'
-import { Row, Col, Card } from 'antd'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Card, Progress, Tag } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
 import MenuFooter from '../../../layouts/components/footer'
+import moment from 'moment'
+import { ArrowRightOutlined } from '@ant-design/icons'
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryTheme,
+} from 'victory'
+import { RiSearch2Fill } from 'react-icons/ri'
+import { getAllReviewsByPatientAction } from '../../../redux/actions/reviewActions'
 
 const PatientDashboard = () => {
   const { user } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const [ok, setOk] = useState(false)
+  const [reviewList, setReviewsList] = useState([])
+
+  useEffect(() => {
+    dispatch(
+      getAllReviewsByPatientAction({
+        setOk,
+        setReviewsList,
+        patientNHSNumber: user?.nhsNumber,
+      })
+    )
+  }, [dispatch, ok, user?.nhsNumber])
+
+  // Function to group reviews by month and calculate average ratings
+  const getRatingsByMonth = () => {
+    const monthlyData = {}
+
+    reviewList.forEach((review) => {
+      const month = moment(review.date).format('MMM YYYY')
+      if (!monthlyData[month]) {
+        monthlyData[month] = { totalRating: 0, count: 0 }
+      }
+      monthlyData[month].totalRating += review.rating
+      monthlyData[month].count += 1
+    })
+
+    return Object.keys(monthlyData).map((month) => ({
+      month,
+      rating: monthlyData[month].totalRating / monthlyData[month].count,
+    }))
+  }
+
+  const ratingsData = getRatingsByMonth()
+
+  // Calculate average rating
+  const averageRating =
+    reviewList.length > 0
+      ? reviewList.reduce((acc, item) => acc + item.rating, 0) /
+        reviewList.length
+      : 0
 
   return (
     <div>
-      <section style={{ minHeight: '80vh' }}>
+      <section style={{ minHeight: '80vh', marginBottom: '20px' }}>
         <Row gutter={[32, 0]}>
           <Col span={24}>
             <h2
@@ -19,37 +71,101 @@ const PatientDashboard = () => {
             </h2>
           </Col>
         </Row>
-        <Row gutter={[32, 0]}>
-          <Col span={24}>
-            <h2
-              style={{
-                marginLeft: '10px',
-              }}>
-              Please select what you want to do...
-            </h2>
-          </Col>
-        </Row>
         <div className='site-card-wrapper'>
+          {/* First Row: Find a Doctor and Last Active */}
           <Row gutter={16} justify='center'>
-            <Col span={8}>
+            <Col span={12}>
               <Card
                 hoverable
-                onClick={() => (window.location.href = '/patient/find-doctor')}
-                title='Find a Doctor'
-                bordered={false}
-                style={{ height: '100%' }}>
-                Search for a doctor and check out reviews, ratings, and more.
+                style={{ cursor: 'pointer' }}
+                onClick={() => (window.location.href = '/patient/find-doctor')}>
+                <Row align='middle'>
+                  <Col span={4}>
+                    <RiSearch2Fill
+                      style={{ fontSize: '32px', color: 'blue' }}
+                    />
+                  </Col>
+                  <Col span={16} style={{ textAlign: 'center' }}>
+                    <h2>Find a Doctor </h2>
+                  </Col>
+                  <Col span={4} style={{ textAlign: 'right' }}>
+                    <ArrowRightOutlined />
+                  </Col>
+                </Row>
               </Card>
             </Col>
-            <Col span={8}>
-              <Card
-                hoverable
-                title='Review a Doctor'
-                bordered={false}
-                style={{ height: '100%' }}>
-                Look into your past appointments and review your experience with
-                the doctor.
+            <Col span={12}>
+              <Card hoverable bordered={false} style={{ height: '100%' }}>
+                <p>Your last activity was on:</p>
+                <Tag color='blue'>
+                  {moment(user?.lastActive).format('MMMM Do YYYY, h:mm:ss a')}
+                </Tag>
               </Card>
+            </Col>
+          </Row>
+
+          {/* Second Row: Rating History and Average Ratings */}
+          <Row gutter={16} style={{ marginTop: '24px' }}>
+            <Col span={12}>
+              {reviewList.length > 0 ? (
+                <Card>
+                  <h3>Rating History</h3>
+                  <VictoryChart
+                    theme={VictoryTheme.material}
+                    domainPadding={20}
+                    height={250}
+                    padding={{ top: 20, bottom: 50, left: 50, right: 50 }}>
+                    <VictoryAxis
+                      tickFormat={ratingsData.map((item) => item.month)}
+                      style={{ tickLabels: { fontSize: 10, angle: -45 } }}
+                    />
+                    <VictoryAxis
+                      dependentAxis
+                      tickFormat={(x) => `${x}`}
+                      style={{ tickLabels: { fontSize: 10 } }}
+                    />
+                    <VictoryBar
+                      data={ratingsData}
+                      x='month'
+                      y='rating'
+                      labels={({ datum }) =>
+                        `Rating: ${datum.rating.toFixed(1)}`
+                      }
+                      labelComponent={<VictoryTooltip />}
+                      style={{
+                        data: { fill: '#4c72b0' },
+                      }}
+                    />
+                  </VictoryChart>
+                </Card>
+              ) : (
+                <Card title='Rating History'>
+                  <p>No reviews history available.</p>
+                </Card>
+              )}
+            </Col>
+            <Col span={12}>
+              {reviewList.length > 0 ? (
+                <Card title='Average Rating'>
+                  <p>Your average rating given to doctors is shown below</p>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Progress
+                      type='circle'
+                      percent={averageRating * 20} // Assuming ratings are out of 5, scale to 100%
+                      format={() => `${averageRating.toFixed(1)} / 5`}
+                      width={150} // Adjust size to make it larger
+                      strokeWidth={10} // Adjust the stroke width
+                    />
+                  </div>
+                </Card>
+              ) : (
+                <Card title='Average Rating'>
+                  <p>
+                    No average rating as you have not made any reviews till
+                    date.
+                  </p>
+                </Card>
+              )}
             </Col>
           </Row>
         </div>
